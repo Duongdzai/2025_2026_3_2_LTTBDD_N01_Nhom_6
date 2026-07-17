2e67e8baa3194292c6b6e0e8fcecce6b32fb9310
ntvzjnk06 <24100350@st.phenikaa-uni.edu.vn>
2026-07-17 20:31:26 +0700

    feat: tao model CongViec va widget TheCongViec

--- lib/models/cong_viec.dart
+++ lib/models/cong_viec.dart
@@ -1,1 +1,14 @@
+// CongViec.dart
+class CongViec {
+  final String id;
+  final String tieuDe;
+  final String thoiGian;
+  bool daHoanThanh;
 
+  CongViec({
+    required this.id,
+    required this.tieuDe,
+    required this.thoiGian,
+    this.daHoanThanh = false,
+  });
+}
--- lib/widgets/the_cong_viec.dart
+++ lib/widgets/the_cong_viec.dart
@@ -1,1 +1,36 @@
+// lib/widgets/the_cong_viec.dart
+import 'package:flutter/material.dart';
+import '../models/cong_viec.dart';
 
+class TheCongViec extends StatelessWidget {
+  final CongViec congViec;
+  final VoidCallback onTap; // Nhận sự kiện bấm từ ngoài truyền vào
+
+  const TheCongViec({
+    super.key,
+    required this.congViec,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: 6, horizontal: 16.0),
+      child: ListTile(
+        onTap: onTap, // Bấm vào cả dòng để đổi trạng thái
+        leading: Icon(
+          congViec.daHoanThanh ? Icons.check_circle : Icons.radio_button_unchecked,
+          color: congViec.daHoanThanh ? Colors.green : Colors.grey,
+        ),
+        title: Text(
+          congViec.tieuDe,
+          style: TextStyle(
+            decoration: congViec.daHoanThanh ? TextDecoration.lineThrough : null,
+            color: congViec.daHoanThanh ? Colors.grey : null,
+          ),
+        ),
+        subtitle: Text('Công việc • ${congViec.thoiGian}'),
+      ),
+    );
+  }
+}
