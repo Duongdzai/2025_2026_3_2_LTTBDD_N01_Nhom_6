29c46e07ebd88f2251655b339d1b1b2d6b096592
Duongdzai <duonglunnn006@gmail.com>
2026-07-17 12:22:44 +0700

    feat: xây dựng giao diện chính danh sách công việc tĩnh

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,38 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(const MyApp());
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    return MaterialApp(
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData.dark(),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Quản lý Công việc')),
+        body: ListView(
+          children: const [
+            ListTile(
+              title: Text('Hoàn thành báo cáo tuần'),
+              subtitle: Text('Công việc • 18/7/2026'),
+            ),
+            ListTile(
+              title: Text('Ôn tập môn Lập trình Di động'),
+              subtitle: Text('Học tập • 19/7/2026'),
+            ),
+            ListTile(
+              title: Text('Gọi điện nói chuyện với bạn'),
+              subtitle: Text('Cá nhân • Ngày mai'),
+            ),
+            ListTile(
+              title: Text('Mua sắm cuối tuần'),
+              subtitle: Text('Mua sắm • 21/7/2026'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
